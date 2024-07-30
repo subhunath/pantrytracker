eac968eff420e14b004075d81348c31ae979107b
Belalaskaik <belalaskaik@gmail.com>
2024-07-30 13:22:34 -0700

    another push

--- app/page.js
+++ app/page.js
@@ -1,95 +1,55 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import { Box, Stack, Typography } from "@mui/material";
+const items = ['tomato', 'potato', 'onion', 'garlic', 'ginger', 'carrot', 'lettuce', 'kale', 'cucumber']
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <Box width="100vw" height="100vh"
+      display={'flex'}
+      justifyContent={'center'}
+      alignItems={'center'}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      flexDirection={"column"}
+      >
+        <Box border={'1px solid  #333'}>
+          
+        <Box width="800px" 
+        height="100px" 
+        bgcolor="#ADD8E6" 
+        display="flex" 
+        justifyContent="center" 
+        alignItems="center"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+          <Typography variant="h2" color="#333" fontWeight="bold">
+            Pantry Tracker
+          </Typography>
+        </Box>
+        <Stack width="800px" 
+        height="300px" 
+        spacing={2} 
+        overflow={'auto'} 
         >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+          {items.map((i) => (
+            <Box 
+            key={i}
+            width="100%"
+            height="300px"
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+            bgcolor={'#f0f0f0'}>
+            <Typography
+            variant={'h3'}
+            color={'#333'}
+            textAlign={'center'}
+            >
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            {
+            i.charAt(0).toUpperCase() + i.slice(1)
+            }
+            </Typography>
+            </Box>
+          ))}
+        </Stack>
+      </Box>
+    </Box>
   );
 }
